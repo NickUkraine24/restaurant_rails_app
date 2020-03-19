92950aea658072458175dcf6da7ea405043588f1
Mykola Ilashchuk <80954980827ni@gmail.com>
2020-03-19 17:28:16 +0200

    Connected ReactJS to project and wrote example of creating component with controller such as Admin

---
+++ app/javascript/components/Admin.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Admin extends React.Component {
+  render () {
+    return (
+        <div>
+          <div>Meat: {this.props.meat}</div>
+        </div>
+    );
+  }
+}
+
+Admin.propTypes = {
+  meat: PropTypes.string
+};
+export default Admin;
