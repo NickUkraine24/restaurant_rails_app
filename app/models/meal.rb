10f3ccaf0dba3dbbae8b7e32b4bd37f0d5a53cf1
Mykola Ilashchuk <80954980827ni@gmail.com>
2020-03-20 18:48:28 +0200

    merged with development and changed validation for price

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,6 @@
 class Meal < ApplicationRecord
   validates :name, presence: true, length: { in: 3..60 }, uniqueness: true
-  validates :price, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   has_and_belongs_to_many :ingredients
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_082417) do
+ActiveRecord::Schema.define(version: 2020_03_20_140953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 2020_03_20_082417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-end
-
-ActiveRecord::Schema.define(version: 2020_03_20_140953) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "meals", force: :cascade do |t|
     t.string "name", limit: 60
