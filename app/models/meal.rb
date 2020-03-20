991994a21039cff742f5c119c26b4f65904d6940
Mykola Ilashchuk <80954980827ni@gmail.com>
2020-03-20 19:04:09 +0200

    changed validation of meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,7 @@
 class Meal < ApplicationRecord
   validates :name, presence: true, length: { in: 3..60 }, uniqueness: true
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true, length: { maximum: 200 }
 
   has_and_belongs_to_many :ingredients
 end
