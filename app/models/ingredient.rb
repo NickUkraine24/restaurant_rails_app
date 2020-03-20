cc3eeacfee7a52b5280902bce006183afbc12e79
aukra <aukrainets@teladoc.com>
2020-03-20 15:49:51 +0200

    add Ingredient model

---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ApplicationRecord
+
+  validates :name, presence: true, length: { in: 3..60 }, uniqueness: true
+  validates :priority, numericality: { only_integer: true, greater_than: 0}
+
+end
